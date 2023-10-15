3a38638025a738e085a875dfe96c89927e1f9e86
Romaric Zounlome <rzounlom@gmail.com>
2023-10-15 17:25:50 -0400

    add car service to retrieve cars from API

--- js/index.js
+++ js/index.js
@@ -10,4 +10,28 @@ $(document).ready(function () {
       $(".navbar #navbarNav ul li a").removeClass("scrolled");
     }
   });
+
+  // Example usage:
+  const carService = new CarService();
+  console.log(carService);
+
+  // Fetch all cars and render them
+  carService
+    .getAllCars()
+    .done((cars) => {
+      console.log(cars);
+      carService.renderCars(cars);
+    })
+    .fail((error) => {
+      console.log("Error fetching cars:", error);
+    });
+
+  // To create a new car (example data provided)
+  // carService.createCar({ name: 'New Car', imgUrl: 'url_here', details: 'Car details here' });
+
+  // To update a car with ID 1 (example data provided)
+  // carService.updateCar(1, { name: 'Updated Car Name' });
+
+  // To delete a car with ID 1
+  // carService.deleteCar(1);
 });
